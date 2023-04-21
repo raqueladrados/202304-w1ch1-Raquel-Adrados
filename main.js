d0dfa3c8db38a888b2047fc188529c3a51b198af
Raquel Adrados <radrados00@gmail.com>
2023-04-21 11:40:12 +0200

    changes in html button restart, js function of setpiece includes vertical count and incorporate the restarButton functio)

--- main.js
+++ main.js
@@ -1,24 +1,81 @@
 let player1 = "Red";
 let player2 = "Yellow";
 let currentPlayer = player1;
-let verticalCount = 0;
+let p1VerticalCount = [
+  {
+    column: 1,
+    consecutivePieces: 0,
+  },
+  {
+    column: 2,
+    consecutivePieces: 0,
+  },
+  {
+    column: 3,
+    consecutivePieces: 0,
+  },
+  {
+    column: 4,
+    consecutivePieces: 0,
+  },
+  {
+    column: 5,
+    consecutivePieces: 0,
+  },
+  {
+    column: 6,
+    consecutivePieces: 0,
+  },
+  {
+    column: 7,
+    consecutivePieces: 0,
+  },
+];
+let p2VerticalCount = [
+  {
+    column: 1,
+    consecutivePieces: 0,
+  },
+  {
+    column: 2,
+    consecutivePieces: 0,
+  },
+  {
+    column: 3,
+    consecutivePieces: 0,
+  },
+  {
+    column: 4,
+    consecutivePieces: 0,
+  },
+  {
+    column: 5,
+    consecutivePieces: 0,
+  },
+  {
+    column: 6,
+    consecutivePieces: 0,
+  },
+  {
+    column: 7,
+    consecutivePieces: 0,
+  },
+];
 
 const setPiece = (event) => {
-  console.log(event);
   let columnSelected = event.target.className.charAt(1); //50
   console.log(columnSelected);
 
   for (row = 1; row <= 6; row++) {
     let currentRow = document.querySelector(`.c${columnSelected}${row}`);
-    console.log(document.querySelector(`.c${columnSelected}${row}`));
-    verticalCount++;
 
     if (
       currentRow.style.backgroundColor === "white" &&
       currentPlayer === player1
     ) {
       currentRow.style.backgroundColor = "red";
-      verticalCount = 0;
+      p2VerticalCount[columnSelected - 1].consecutivePieces = 0;
+      p1VerticalCount[columnSelected - 1].consecutivePieces++;
       document.querySelector(`.currentPlayer`).style.backgroundColor = "yellow";
       return (currentPlayer = player2);
     }
@@ -28,30 +85,46 @@ const setPiece = (event) => {
       currentPlayer === player2
     ) {
       currentRow.style.backgroundColor = "yellow";
-      verticalCount = 0;
+      p1VerticalCount[columnSelected - 1].consecutivePieces = 0;
+      p2VerticalCount[columnSelected - 1].consecutivePieces++;
       document.querySelector(`.currentPlayer`).style.backgroundColor = "red";
       return (currentPlayer = player1);
     }
-
-    checkWinner();
   }
 };
 
 const startGame = () => {
+  console.log("Juego iniciado");
   for (index = 1; index <= 7; index++) {
     let column = document.querySelector(`.c${index}`);
-    column.addEventListener("click", setPiece);
+    column.addEventListener("click", () => {
+      setPiece(event);
+      console.log("checking");
+      checkWinner();
+    });
   }
 };
+
+const checkWinner = () => {
+  for (index = 0; index < p1VerticalCount.length; index++) {
+    if (p1VerticalCount[index].consecutivePieces === 4) {
+      console.log("El jugador 1 ha ganado!");
+    }
+  }
+
+  for (index = 0; index < p2VerticalCount.length; index++) {
+    if (p2VerticalCount[index].consecutivePieces === 4) {
+      console.log("El jugador 2 ha ganado!");
+    }
+  }
+};
+
+let startButton = document.querySelector(".start");
+startButton.addEventListener("click", startGame);
 
 const restartGame = () => {
-  for (index = 1; index <= 7; index++) {
-    let column = document.querySelector(`.c${index}`);
-    column.addEventListener("click", startGame);
-  }
+  location.reload();
 };
 
-let restartButton = document.querySelector(".buttonContent");
-
-let startButton = document.querySelector(".buttonContent");
-startButton.addEventListener("click", startGame);
+let restartButton = document.querySelector(".reload");
+restartButton.addEventListener("click", restartGame);
